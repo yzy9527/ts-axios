cc755320e735092aaf238e0945e03677b12d9cf7
yzy9527 <cyzy9527@163.com>
2021-04-11 23:55:56 +0800

    feat: 响应结果添加泛型

--- examples/extend/app.ts
+++ examples/extend/app.ts
@@ -1,34 +1,34 @@
 import axios from '../../src/index'
 
-axios({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hi'
-  }
-})
-
-axios.request({
-  url: '/extend/post',
-  method: 'post',
-  data: {
-    msg: 'hello'
-  }
-})
-
-axios.get('/extend/get')
-
-axios.options('/extend/options')
-
-axios.delete('/extend/delete')
-
-axios.head('/extend/head')
-
-axios.post('/extend/post', { msg: 'post' })
-
-axios.put('/extend/put', { msg: 'put' })
-
-axios.patch('/extend/patch', { msg: 'patch' })
+// axios({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hi'
+//   }
+// })
+//
+// axios.request({
+//   url: '/extend/post',
+//   method: 'post',
+//   data: {
+//     msg: 'hello'
+//   }
+// })
+//
+// axios.get('/extend/get')
+//
+// axios.options('/extend/options')
+//
+// axios.delete('/extend/delete')
+//
+// axios.head('/extend/head')
+//
+// axios.post('/extend/post', { msg: 'post' })
+//
+// axios.put('/extend/put', { msg: 'put' })
+//
+// axios.patch('/extend/patch', { msg: 'patch' })
 
 // axios({
 //   url: '/extend/post',
@@ -41,33 +41,33 @@ axios.patch('/extend/patch', { msg: 'patch' })
 // axios('/extend/post', {
 //   method: 'post',
 //   data: {
-//     msg: 'hello'
+//     msg: 'hello0'
 //   }
 // })
 
-// interface ResponseData<T = any> {
-//   code: number
-//   result: T
-//   message: string
-// }
-//
-// interface User {
-//   name: string
-//   age: number
-// }
-//
-// function getUser<T>() {
-//   return axios<ResponseData<T>>('/extend/user')
-//     .then(res => res.data)
-//     .catch(err => console.error(err))
-// }
-//
-//
-// async function test() {
-//   const user = await getUser<User>()
-//   if (user) {
-//     console.log(user.result.name)
-//   }
-// }
-//
-// test()
+interface ResponseData<T = any> {
+  code: number
+  result: T
+  message: string
+}
+
+interface User {
+  name: string
+  age: number
+}
+
+function getUser<T>() {
+  return axios<ResponseData<T>>('/extend/user')
+    .then(res => res.data)
+    .catch(err => console.error(err))
+}
+
+
+async function test() {
+  const user = await getUser<User>()
+  if (user) {
+    console.log(user.result.name)
+  }
+}
+
+test()
